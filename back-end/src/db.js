9904d29e969d42543b1847df1d13c6df692e9a4d
Anup Kumar <anupkumar1803@gmail.com>
2023-02-15 16:25:09 +0530

    UserAuthentication Basic

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -1,9 +1,9 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb'
 
 let client;
 
 export const initializeDbConnection = async () => {
-    client = await MongoClient.connect('mongodb://localhost:27017', {
+    client = await MongoClient.connect('mongodb+srv://Anupkumar:TataMarch18@cluster0.3clsfrl.mongodb.net/?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,11 @@
+import { logInRoute } from './loginRoute';
+import { signUpRoute } from './signUpRoute';
 import { testRoute } from './testRoute';
+import { updateUserInfoRoute } from './updateUserInfoRoutes';
 
 export const routes = [
+    logInRoute,
+    signUpRoute,
+    updateUserInfoRoute,
     testRoute,
 ];
